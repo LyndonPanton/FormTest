4f44e5f373191fe8564770ac8f3824097409c0b7
Lyndon Panton <LyndonPanton@gmail.com>
2019-05-23 20:23:45 +0100

    Changed color of text based on slider value

--- js/app.js
+++ js/app.js
@@ -6,4 +6,24 @@
         
         alert("Thank you for your submission, you will be contacted shortly");
     });
+    
+    let happinessMeter = document.getElementById("form-happiness");
+    happinessMeter.addEventListener("input", function(event) {
+        document.getElementById("form-happiness-output").textContent = this.value;
+        if (this.value < 14) {
+            document.getElementById("form-happiness-output").style.color = "red";
+        } else if (this.value < 28) {
+            document.getElementById("form-happiness-output").style.color = "orange";
+        } else if (this.value < 42) {
+            document.getElementById("form-happiness-output").style.color = "yellow";
+        } else if (this.value < 56) {
+            document.getElementById("form-happiness-output").style.color = "green";
+        } else if (this.value < 70) {
+            document.getElementById("form-happiness-output").style.color = "blue";
+        } else if (this.value < 84) {
+            document.getElementById("form-happiness-output").style.color = "indigo";
+        } else if (this.value >= 84) {
+            document.getElementById("form-happiness-output").style.color = "violet";
+        }
+    });
 }());
